71834dfbe424ed2b2dc4a3f3d5b37a6a5aab3c1f
unknown <rtribbia@E5440-170CL12.ntdom.cupdx>
2015-05-22 09:44:35 -0700

    new update

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+
 app.get('/', function(req, res){
   console.log(req);
   res.sendFile(__dirname + '/public/index.html');
